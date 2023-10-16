32ab2befd63b5f62aa0e9a8deee10ab09a7ba6dd
Mohamed <46447150+MohamedAbdelhalem@users.noreply.github.com>
2023-10-16 08:48:20 +0300

    Delete AlwaysOn/active_node.sql

--- AlwaysOn/active_node.sql
+++
@@ -1,6 +0,0 @@
-select ag.name [availability_group], role, role_desc, avgs.primary_replica
-from sys.dm_hadr_availability_replica_states avrs inner join sys.availability_groups ag
-on avrs.group_id = ag.group_id
-inner join sys.dm_hadr_availability_group_states avgs
-on avgs.group_id = ag.group_id
-where is_local = 1
