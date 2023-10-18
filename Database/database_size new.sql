1429d5afd04a6904ec5a940af80c914a9effe2c6
Mohamed <46447150+MohamedAbdelhalem@users.noreply.github.com>
2023-10-18 08:51:59 +0300

    Delete Database/database_size new.sql

--- Database/database_size new.sql
+++
@@ -1,9 +0,0 @@
-Exec [master].[dbo].[database_size]
-@databases		= '*',
-@with_system	= 1,
-@threshold_pct	= 85,
-@volumes		= '*',
-@where_size_gt  = 0,
-@datafile		= '*',
-@report			= 1,
-@over_threshold = 0
