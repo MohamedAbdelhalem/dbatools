016dde72d1ab1743d4655b035e60d45efff645a1
Mohamed <46447150+MohamedAbdelhalem@users.noreply.github.com>
2023-11-01 23:17:38 +0300

    Update 3- Change_Vote_for_all_Nodes.sql

--- AlwaysOn/DR Switch/3- Change_Vote_for_all_Nodes.sql
+++ AlwaysOn/DR Switch/3- Change_Vote_for_all_Nodes.sql
@@ -1,7 +1,19 @@
 --Enable SQLCMD mode first
 :CONNECT 10.55.20.1
-  
+
+exec sp_configure 'show advanced options',1
+go
+reconfigure 
+go
+exec sp_configure 'xp_cmdshell',1
+go
+reconfigure 
+go
+
 exec xp_cmdshell 'powershell.exe -Command "& {(get-ClusterNode D1SQLDBPrWV1).NodeWeight=0}"'
+go
 exec xp_cmdshell 'powershell.exe -Command "& {(get-ClusterNode D1SQLDBPrWV2).NodeWeight=0}"'
+go
 exec xp_cmdshell 'powershell.exe -Command "& {(get-ClusterNode D2SQLDBDrWV1).NodeWeight=1}"'
+go
 exec xp_cmdshell 'powershell.exe -Command "& {(get-ClusterNode D2SQLDBDrWV2).NodeWeight=1}"'
