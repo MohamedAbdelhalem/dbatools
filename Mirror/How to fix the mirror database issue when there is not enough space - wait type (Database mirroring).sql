33f71497168ded36d21fa12a2312d47bc3a3fda8
Mohamed <46447150+MohamedAbdelhalem@users.noreply.github.com>
2023-11-02 13:04:38 +0300

    Update How to fix the mirror database issue when there is not enough space - wait type (Database mirroring).sql

--- Mirror/How to fix the mirror database issue when there is not enough space - wait type (Database mirroring).sql
+++ Mirror/How to fix the mirror database issue when there is not enough space - wait type (Database mirroring).sql
@@ -1,6 +1,6 @@
 --From the Primary node
 
-ALTER DATABASE [AptraConn_prd] SET PARTNER OFF;
+ALTER DATABASE [Database_name] SET PARTNER OFF;
 
 --From Secondary node and make sure that Log Send Queue KB counter doesnt have any queues
 go
@@ -10,7 +10,7 @@ insert into @table
 select object_name, counter_name, instance_name, cntr_value
 from sys.dm_os_performance_counters
 where object_name like '%mirr%'
-and instance_name = 'AptraConn_prd'
+and instance_name = 'Database_name'
 and counter_name not like '%/%'
 and counter_name in ('Log Send Queue KB')
 and cntr_value > 0 
@@ -28,19 +28,19 @@ order by counter_name
 
 --Then take transaction log backup from the Primary database
 go
-BACKUP log [AptraConn_prd] TO  DISK = N'\\npci1.d1fs.albilad.com\SQLNativeBackup\D1APTDBSQPWV1\AptraConn_prd_log_2023_07_25__03_23_pm.bak' WITH NOFORMAT, NOINIT,  
-NAME = N'AptraConn_prd-log Database Backup', SKIP, NOREWIND, NOUNLOAD, COMPRESSION, STATS = 1
+BACKUP log [Database_name] TO  DISK = N'\\BackupPath\Database_name_log_2023_07_25__03_23_pm.bak' WITH NOFORMAT, NOINIT,  
+NAME = N'Database_name-log Database Backup', SKIP, NOREWIND, NOUNLOAD, COMPRESSION, STATS = 1
 
 --Then restore it on the Secondary node
 go
-RESTORE LOG [AptraConn_prd]
-FROM DISK = N'\\npci1.d1fs.albilad.com\SQLNativeBackup\D1APTDBSQPWV1\AptraConn_prd_log_2023_07_25__03_23_pm.bak'
+RESTORE LOG [Database_name]
+FROM DISK = N'\\BackupPath\Database_name_log_2023_07_25__03_23_pm.bak'
 WITH FILE = 1,
-NAME = N'AptraConn_prd-log Database Backup', NORECOVERY, NOUNLOAD, STATS = 1
+NAME = N'Database_name-log Database Backup', NORECOVERY, NOUNLOAD, STATS = 1
 
 --Then go to the Secondary node and join the database to mirror
 go
-declare @partner varchar(500), @set_partner varchar(1000), @database_name varchar(500) = 'AptraConn_prd'
+declare @partner varchar(500), @set_partner varchar(1000), @database_name varchar(500) = 'Database_name'
 select top 1 @partner = mirroring_partner_name 
 from sys.database_mirroring
 where mirroring_partner_name is not null
@@ -50,7 +50,7 @@ exec(@set_partner)
 
 --And finally go to the Primary node and join the database to mirror
 go
-declare @partner varchar(500), @set_partner varchar(1000), @database_name varchar(500) = 'AptraConn_prd'
+declare @partner varchar(500), @set_partner varchar(1000), @database_name varchar(500) = 'Database_name'
 select top 1 @partner = mirroring_partner_name 
 from sys.database_mirroring
 where mirroring_partner_name is not null
