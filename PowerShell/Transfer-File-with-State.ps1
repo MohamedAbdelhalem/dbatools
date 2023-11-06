8d845848a80af00d6a42f9937e0e0e2931bcbf96
Mohamed <46447150+MohamedAbdelhalem@users.noreply.github.com>
2023-11-06 15:08:38 +0300

    Update Transfer-File-with-State.ps1

--- PowerShell/Transfer-File-with-State.ps1
+++ PowerShell/Transfer-File-with-State.ps1
@@ -1,6 +1,6 @@
 Function File-Transfer-with-state 
 {
-param( [string]$filename, [string]$PathFrom, [string]$PathTo)
+param( [string]$filename, [int]$speed_MB, [string]$PathFrom, [string]$PathTo)
 $full_from_file = $PathFrom+"\"+$filename
 $full_to_file = $Pathto+"\"+$filename
 $ffile = [io.file]::OpenRead($full_from_file)
@@ -14,10 +14,9 @@ to ""$full_to_file"""
 ""
 
 try {
-    [byte[]]$buff = new-object byte[] 1048576 # 1 MB speed
+    [byte[]]$buff = new-object byte[] ($speed_MB * 1MB) # speed by MB
     [long]$total = [int]$count = 0
     do {
-
         #[string][math]::round(($total/$total_bytes) * 100.0,1) +"%"
         $count = $ffile.Read($buff, 0, $buff.Length)
         $tofile.Write($buff, 0, $count)
@@ -31,5 +30,6 @@ try {
      }
 }
 
-File-Transfer-with-state -filename 'SSMS-Setup-ENU.exe' -PathFrom 'N:\must_delete' -PathTo 'N:\must_delete_temp'
+File-Transfer-with-state -filename 'T24SDC3_HIS_conv_2023_09_14__01_13_pm.bak' -speed_MB 4 -PathFrom 'N:\must_delete' -PathTo 'N:\must_delete_temp'
+
 
