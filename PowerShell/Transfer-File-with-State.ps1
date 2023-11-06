caf91db442bbd06e33dbd7f10c0737bf1e1f5276
Mohamed <46447150+MohamedAbdelhalem@users.noreply.github.com>
2023-11-06 12:10:03 +0300

    Update Transfer-File-with-State.ps1

--- PowerShell/Transfer-File-with-State.ps1
+++ PowerShell/Transfer-File-with-State.ps1
@@ -1,28 +1,32 @@
-function File-Transfer-with-states {
-    param( [string]$originalFile, [string]$copyFile)
-$ffile = [io.file]::OpenRead($originalFile)
-$tofile = [io.file]::OpenWrite($copyFile)
+Function File-Transfer-with-state {
+    param( [string]$filename, [string]$PathFrom, [string]$PathTo)
+        $full_from_file = $PathFrom+"\"+$filename
+        $full_to_file = $Pathto+"\"+$filename
+        $ffile = [io.file]::OpenRead($full_from_file)
+        $tofile = [io.file]::OpenWrite($full_to_file)
 
-$byteData = Get-Content -Path $filePath -Encoding Byte -ReadCount 0 -Raw | foreach { $_ }
-$total_bytes = $byteData.Count
-"Copy ""$filePath"" to ""$outputFile"" shows with below progress state."
-""
-try {
-    [byte[]]$buff = new-object byte[] 4096
-    [long]$total = [int]$count = 0
-    do {
-        [string][math]::round(($total/$total_bytes) * 100.0,1) +"%"
- 
-        $count = $ffile.Read($buff, 0, $buff.Length)
-        $tofile.Write($buff, 0, $count)
-        $total += $count
+        $byteData = Get-Content -Path $full_from_file -Encoding Byte -ReadCount 0 -Raw | foreach { $_ }
+        $total_bytes = $byteData.Count
+
+        "Copy from ""$full_from_file"" to ""$full_from_file"" shows with below progress state."
+        ""
+        try {
+                [byte[]]$buff = new-object byte[] 262144 #256 kb speed
+                [long]$total = [int]$count = 0
+                do {
+                    #[string][math]::round(($total/$total_bytes) * 100.0,1) +"%"
+                    Write-Progress -PercentComplete ($total/$total_bytes*100) -Status "Coping file progress" -Activity "Item $item of $total_bytes"
+
+                    $count = $ffile.Read($buff, 0, $buff.Length)
+                    $tofile.Write($buff, 0, $count)
+                    $total += $count
         
-    } while ($count -gt 0)
-} finally {
-    $ffile.Dispose()
-    $tofile.Dispose()
-}
-}
+                    } while ($count -gt 0)
+            } finally {
+            $ffile.Dispose()
+            $tofile.Dispose()
+                       }
+    }
 
-File-Transfer-with-states -originalFile 'C:\temp\Untitled_Recovered.gif' -copyFile 'K:\Backup\Untitled_Recovered.gif'
+File-Transfer-with-state -filename 'SSMS-Setup-ENU.exe' -PathFrom 'C:\temp\new\' -PathTo '\\npci2.d2fs.albilad.com\DBTEMP\T24DBXTREMIOT4'
 
