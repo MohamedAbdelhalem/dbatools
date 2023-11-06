de44d0d18da56591f7def6bb95d26877977a6bcf
Mohamed <46447150+MohamedAbdelhalem@users.noreply.github.com>
2023-11-06 18:45:09 +0300

    Update File-Transfer-with-state.ps1

--- PowerShell/File-Transfer-with-state.ps1
+++ PowerShell/File-Transfer-with-state.ps1
@@ -7,6 +7,9 @@ $ffile = [io.file]::OpenRead($full_from_file)
 $tofile = [io.file]::OpenWrite($full_to_file)
 
 $total_bytes = (((get-item $full_from_file).Length/1MB) * 1024 *1024)
+$startDate = (Get-Date)
+#[datetime]$time_to_complet
+[datetime]$date = "2023-01-01 00:00:00"
 
 "Copying file... 
 from ""$full_from_file"" 
@@ -21,7 +24,14 @@ try {
         $count = $ffile.Read($buff, 0, $buff.Length)
         $tofile.Write($buff, 0, $count)
         $total += $count
-        Write-Progress -PercentComplete (($total/$total_bytes)*100.0) -Status "In-progress..." -Activity "Coping file $filename"
+        #cast((100.0 / (round(@percent_complete,15) + .00001)) * datediff(s, @start_time, getdate()) as int) - datediff(s, @start_time, getdate())
+        $seconds = ((Get-Date) - $startDate).totalSeconds
+
+        $second_to_complete = ((100.0 / (($total/$total_bytes)*100.0)) * $seconds) - $seconds
+        $time_to_complet = $date.AddSeconds($second_to_complete)
+        [string]$print_time = $time_to_complet.ToString('HH:mm:ss') 
+
+        Write-Progress -PercentComplete (($total/$total_bytes)*100.0) -Status "waiting $print_time to complete" -Activity "Coping file $filename"
         } 
     while ($count -gt 0)
        } finally {
@@ -30,6 +40,7 @@ try {
      }
 }
 
-File-Transfer-with-state -filename 'T24SDC3_HIS_conv_2023_09_14__01_13_pm.bak' -speed_MB 4 -PathFrom 'N:\must_delete' -PathTo 'N:\must_delete_temp'
+
+File-Transfer-with-state -filename 'T24SDC3_HIS_conv_2023_09_14__01_13_pm.bak' -speed_MB 15 -PathFrom 'N:\must_delete' -PathTo 'N:\must_delete_temp'
 
 
