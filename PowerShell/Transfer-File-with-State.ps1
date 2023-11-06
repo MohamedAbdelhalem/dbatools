286c5782b216efa7bf80e7da793085e155624145
Mohamed <46447150+MohamedAbdelhalem@users.noreply.github.com>
2023-11-06 14:35:21 +0300

    Update Transfer-File-with-State.ps1

--- PowerShell/Transfer-File-with-State.ps1
+++ PowerShell/Transfer-File-with-State.ps1
@@ -1,34 +1,35 @@
-Function File-Transfer-with-state {
-    param( [string]$filename, [string]$PathFrom, [string]$PathTo)
-        $full_from_file = $PathFrom+"\"+$filename
-        $full_to_file = $Pathto+"\"+$filename
-        $ffile = [io.file]::OpenRead($full_from_file)
-        $tofile = [io.file]::OpenWrite($full_to_file)
+Function File-Transfer-with-state 
+{
+param( [string]$filename, [string]$PathFrom, [string]$PathTo)
+$full_from_file = $PathFrom+"\"+$filename
+$full_to_file = $Pathto+"\"+$filename
+$ffile = [io.file]::OpenRead($full_from_file)
+$tofile = [io.file]::OpenWrite($full_to_file)
 
-        $byteData = Get-Content -Path $full_from_file -Encoding Byte -ReadCount 0 -Raw | foreach { $_ }
-        $total_bytes = $byteData.Count
+$total_bytes = (((get-item $full_from_file).Length/1MB) * 1024 *1024)
 
-        "Copy from ""$full_from_file"" 
-        to ""$full_to_file"" 
-        shows with below progress state."
-        ""
-        try {
-                [byte[]]$buff = new-object byte[] 1073741824 # 1 MB speed
-                [long]$total = [int]$count = 0
-                do {
-                    #[string][math]::round(($total/$total_bytes) * 100.0,1) +"%"
-                    Write-Progress -PercentComplete ($total/$total_bytes*100) -Status "Coping file progress" -Activity "Item $item of $total_bytes"
+"Copying file... 
+from ""$full_from_file"" 
+to ""$full_to_file""" 
+""
 
-                    $count = $ffile.Read($buff, 0, $buff.Length)
-                    $tofile.Write($buff, 0, $count)
-                    $total += $count
-        
-                    } while ($count -gt 0)
-            } finally {
-            $ffile.Dispose()
-            $tofile.Dispose()
-                       }
-    }
+try {
+    [byte[]]$buff = new-object byte[] 1048576 # 1 MB speed
+    [long]$total = [int]$count = 0
+    do {
 
-File-Transfer-with-state -filename 'SSMS-Setup-ENU.exe' -PathFrom 'C:\temp\new\' -PathTo '\\domain.com\DB\Backup'
+        #[string][math]::round(($total/$total_bytes) * 100.0,1) +"%"
+        $count = $ffile.Read($buff, 0, $buff.Length)
+        $tofile.Write($buff, 0, $count)
+        $total += $count
+        Write-Progress -PercentComplete (($total/$total_bytes)*100.0) -Status "In-progress..." -Activity "Coping file progress"
+        } 
+    while ($count -gt 0)
+       } finally {
+         $ffile.Dispose()
+         $tofile.Dispose()
+     }
+}
+
+File-Transfer-with-state -filename 'SSMS-Setup-ENU.exe' -PathFrom 'N:\must_delete' -PathTo 'N:\must_delete_temp'
 
