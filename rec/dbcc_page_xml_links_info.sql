7d058d2cff4cdc867da06596d1e258230aad0a51
Mohamed <46447150+MohamedAbdelhalem@users.noreply.github.com>
2023-12-19 16:33:16 +0300

    Update dbcc_page_xml_links_info.sql

--- rec/dbcc_page_xml_links_info.sql
+++ rec/dbcc_page_xml_links_info.sql
@@ -1,3 +1,33 @@
+select top 150 
+'dbcc page(0,'+cast(master.[dbo].[virtical_array](replace(replace(sys.fn_PhysLocFormatter (%%physloc%%),')',''),'(',''),':',1) collate Arabic_100_CI_AS as varchar(10))+','+ 
+cast(master.[dbo].[virtical_array](replace(replace(sys.fn_PhysLocFormatter (%%physloc%%),')',''),'(',''),':',2) collate Arabic_100_CI_AS  as varchar(10))+',3) with tableresults',
+master.[dbo].[virtical_array](replace(replace(sys.fn_PhysLocFormatter (%%physloc%%),')',''),'(',''),':',3) slotid,
+RECID, XMLRECORD,
+CONVERT(varbinary(max),XMLRECORD) bcrypt,
+CONVERT(nvarchar(max),CONVERT(varbinary(max),
+STUFF(
+STUFF(
+STUFF(
+STUFF(
+STUFF(
+STUFF(
+STUFF(
+STUFF(
+STUFF(
+CONVERT(varbinary(max),XMLRECORD)
+, 10, 0, substring(CONVERT(varbinary(max),N'X!01M@02L#03R$04E%05C^06O&07R*08D(09'),1,8))
+, 30, 0, substring(CONVERT(varbinary(max),N'X!01M@02L#03R$04E%05C^06O&07R*08D(09'),9,8))
+, 50, 0, substring(CONVERT(varbinary(max),N'X!01M@02L#03R$04E%05C^06O&07R*08D(09'),17,8))
+, 76, 0, substring(CONVERT(varbinary(max),N'X!01M@02L#03R$04E%05C^06O&07R*08D(09'),25,8))
+, 99, 0, substring(CONVERT(varbinary(max),N'X!01M@02L#03R$04E%05C^06O&07R*08D(09'),33,8))
+,119, 0, substring(CONVERT(varbinary(max),N'X!01M@02L#03R$04E%05C^06O&07R*08D(09'),41,8))
+,140, 0, substring(CONVERT(varbinary(max),N'X!01M@02L#03R$04E%05C^06O&07R*08D(09'),49,8))
+,169, 0, substring(CONVERT(varbinary(max),N'X!01M@02L#03R$04E%05C^06O&07R*08D(09'),57,8))
+,189, 0, substring(CONVERT(varbinary(max),N'X!01M@02L#03R$04E%05C^06O&07R*08D(09'),65,8))
+),2) acrypt
+FROM [dbo].[F_BAB_H_O205]
+
+
 dbcc traceon(3604)
 declare @dbcc_page table (id int identity(1,1), ParentObject varchar(1000), Object varchar(1000), Field varchar(1000), VALUE varchar(1000)) 
 declare @dbcc_page_links table (slot varchar(10), xml_pages varchar(1000), Field varchar(1000), object varchar(30), ClusterKey_VALUE varchar(1000)) 
