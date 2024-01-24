1a263ba98756ad97f115b6aeaeef42287f64f728
Mohamed <46447150+MohamedAbdelhalem@users.noreply.github.com>
2024-01-24 12:56:24 +0300

    Create VLF_Count_Estimate.sql

---
+++ Database/Transaction Log/VLF_Count_Estimate.sql
@@ -0,0 +1,49 @@
+CREATE Function dbo.VLF_Count_Estimate (
+@intial_size_gb float = 1,
+@growth_size_mb float = 512,
+@target_size_gb float = 200,
+@sql_version	int) 
+returns int
+as
+begin
+declare @vlf int
+
+if @sql_version < 2014
+begin
+
+select @vlf =
+case 
+when @growth_size_mb between 1    and 64   then (((@target_size_gb - @intial_size_gb) * 1024.0) / @growth_size_mb) * 4
+when @growth_size_mb between 65   and 1024 then (((@target_size_gb - @intial_size_gb) * 1024.0) / @growth_size_mb) * 8
+when @growth_size_mb >= 1025			   then (((@target_size_gb - @intial_size_gb) * 1024.0) / @growth_size_mb) * 16
+end 
++
+case 
+when @intial_size_gb * 1024.0 between 1    and 64   then 4
+when @intial_size_gb * 1024.0 between 65   and 1024 then 8
+when @intial_size_gb * 1024.0 >= 1025		 	    then 16
+end
+
+end
+else 
+if @sql_version >= 2014
+begin
+
+select @vlf = case 
+when @intial_size_gb * 1024.0 between 1    and 64   then 4
+when @intial_size_gb * 1024.0 between 65   and 1024 then 8
+when @intial_size_gb * 1024.0 >= 1025		 	    then 16
+end 
+
+while @intial_size_gb < @target_size_gb
+begin
+
+set @vlf = @vlf + IIF((@growth_size_mb / 1024.0) < @intial_size_gb/8.0, 1, case when @growth_size_mb between 1 and 64 then 4 when @growth_size_mb between 65 and 1024 then 8 when @growth_size_mb > 1024 then 16 end)
+set @intial_size_gb = @intial_size_gb + (@growth_size_mb / 1024.0)
+
+end
+end
+
+return @vlf 
+end
+
