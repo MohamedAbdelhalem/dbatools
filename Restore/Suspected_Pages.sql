976a32421609837a644fa4b4d87d1d92b82e9415
Mohamed <46447150+MohamedAbdelhalem@users.noreply.github.com>
2024-02-24 23:55:23 +0300

    Create Suspected_Pages.sql

---
+++ Restore/Suspected_Pages.sql
@@ -0,0 +1,27 @@
+use master
+go
+declare 
+@database varchar(500) = 'AdventureWorks2017',
+@full_bak varchar(max) = 'C:\AdventureWorks\AdventureWorks2017_2024_02_01__19_00_00.bak', 
+@tail_bak varchar(max) = 'C:\AdventureWorks\AdventureWorks2017_TailLog.BAK'
+
+declare @suspected_pages varchar(max)
+select @suspected_pages = ISNULL(@suspected_pages+',','') + CAST(file_id as varchar(10))+':'+CAST(page_id as varchar(50)) 
+from master.dbo.suspect_pages
+where database_id = DB_ID(@database)
+
+--select @suspected_pages
+--1:1178,1:1400,1:1401,1:1405,1:5990,1:5991,1:5992,1:6546
+
+RESTORE DATABASE AdventureWorks2017 
+PAGE = @suspected_pages 
+FROM DISK = @full_bak
+WITH NORECOVERY;
+
+BACKUP LOG AdventureWorks2017 
+TO DISK = @tail_bak
+
+RESTORE LOG AdventureWorks2017 
+FROM DISK = @tail_bak
+WITH RECOVERY;
+
