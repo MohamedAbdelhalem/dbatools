256162c480937118ef71df840e02cfff90cbd7ad
Mohamed <46447150+MohamedAbdelhalem@users.noreply.github.com>
2024-02-26 21:44:08 +0300

    Update fn_executionPlan_params.sql

--- Execution Plan/fn_executionPlan_params.sql
+++ Execution Plan/fn_executionPlan_params.sql
@@ -7,15 +7,16 @@ SET QUOTED_IDENTIFIER ON
 GO
 CREATE Function [dbo].[fn_executionPlan_params]
 (@plan XML)
-returns @table table (id int identity(1,1), bind_variables varchar(1000), parameter_values nvarchar(1000))
+returns @table table (id int identity(1,1), bind_variables varchar(1000), parameter_values nvarchar(1000), ParameterRuntimeValue nvarchar(1000))
 as
 begin
 
 ;WITH xmlnamespaces (default 'http://schemas.microsoft.com/sqlserver/2004/07/showplan')
 insert into @table
 SELECT DISTINCT
-[bind_variables]	= x.value('(@Column)[1]',   'varchar(1000)'),
-[parameters]		= x.value('(@ParameterCompiledValue)[1]', 'nvarchar(1000)')
+[bind_variables]			= x.value('(@Column)[1]',   'varchar(1000)'),
+[parameters]				= x.value('(@ParameterCompiledValue)[1]', 'nvarchar(1000)'),
+[ParameterRuntimeValue]		= x.value('(@ParameterRuntimeValue)[1]', 'nvarchar(1000)')
 FROM   @plan.nodes('//ColumnReference') x1(x)
 where x.value('(@Column)[1]','varchar(1000)') like '%@%'
 and x.value(N'(@ParameterCompiledValue)[1]',N'varchar(1000)') is not null
