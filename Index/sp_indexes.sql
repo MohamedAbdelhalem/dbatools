6d2f568dd4aab9871babc37591c0f7582835d703
Mohamed <46447150+MohamedAbdelhalem@users.noreply.github.com>
2024-03-03 12:51:37 +0300

    Update sp_indexes.sql

--- Index/sp_indexes.sql
+++ Index/sp_indexes.sql
@@ -169,7 +169,7 @@ order by i.key_ordinal
 
 if @is_primary_key = 1
 begin
-set @sql = 'ALTER TABLE '+@p_table_name+' ADD CONSTRAINT ['+@p_index_name+'] PRIMARY KEY '+ case when @index_id = 1 then 'CLUSTERED ' else 'NONCLUSTERED ' end + @sql + 
+set @sql = 'ALTER TABLE '+@p_table_name+' ADD CONSTRAINT ['+@p_index_name+'] PRIMARY KEY '+ case when @P_index_id = 1 then 'CLUSTERED ' else 'NONCLUSTERED ' end + @sql + 
 ') WITH (FILLFACTOR = '+cast(case when @fill_factor = 0 then 100 else @fill_factor end as varchar)+') ON [' + @filegroup +']'+ISNULL('('+@partition_column_name+')','')
 end
 else
@@ -177,7 +177,7 @@ begin
 if @is_unique_constraint = 1
 begin
 
-set @sql = 'ALTER TABLE '+@p_table_name+' ADD CONSTRAINT ['+@p_index_name+'] UNIQUE '+ case when @index_id = 1 then 'CLUSTERED ' else 'NONCLUSTERED ' end + @sql + 
+set @sql = 'ALTER TABLE '+@p_table_name+' ADD CONSTRAINT ['+@p_index_name+'] UNIQUE '+ case when @P_index_id = 1 then 'CLUSTERED ' else 'NONCLUSTERED ' end + @sql + 
 ' ON [' + @filegroup +']'+ISNULL('('+@partition_column_name+')','')
 end
 else
@@ -198,39 +198,6 @@ end, @is_disabled, @column_name, @is_computed, @is_persisted, @compute_function,
 
 end
 
-go
-CREATE Procedure [dbo].[sp_table_indexes]
-(@table_name varchar(500))
-as
-begin
-
-declare @object_id bigint, @index_id bigint
-declare @indexes table (id int identity(1,1), table_name varchar(500), index_id varchar(30), index_name varchar(1000), index_type varchar(100), is_disabled varchar(30), 
-index_columns varchar(1500), column_is_computed varchar(30), column_is_persisted varchar(30), columns_computed_function varchar(500), filegroup varchar(500), fill_factor varchar(30), synatx varchar(max))
-
-declare x cursor fast_forward
-for
-select t.object_id, i.index_id
-from sys.tables t left outer join sys.indexes i
-on t.object_id = i.object_id
-where i.type_desc != 'HEAP'
-and i.object_id = object_id(ltrim(rtrim(@table_name)))
-order by object_id, i.index_id
-
-open x
-fetch next from x into @object_id, @index_id
-while @@FETCH_STATUS = 0
-begin
-
-insert into @indexes (table_name, index_id, index_name, index_type, is_disabled, index_columns, column_is_computed, column_is_persisted, columns_computed_function, filegroup, fill_factor, synatx)
-exec [dbo].[sp_index_details] @P_object_id = @object_id,  @p_index_id = @index_id
-fetch next from x into @object_id, @index_id
-end
-close x
-deallocate x
-
-select * from @indexes
-end
 go
 CREATE Procedure [dbo].[sp_tables_indexes]
 (@table_name varchar(max), @type int = 0)
