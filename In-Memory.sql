31132a50bcd4d6e9e58a54f6a782f1f5f1df0fe0
Mohamed <46447150+MohamedAbdelhalem@users.noreply.github.com>
2024-03-26 23:42:08 +0300

    Create In-Memory.sql

---
+++ In-Memory.sql
@@ -0,0 +1,31 @@
+declare @FilestreamConfiguredLevel int = 2
+
+select 
+SERVERPROPERTY('FilestreamConfiguredLevel') [FileStream_OS_value],
+case SERVERPROPERTY('FilestreamConfiguredLevel')
+when 0 then 'is disabled'
+when 1 then 'is enabled for Transact-SQL access'
+when 2 then 'is enabled for Transact-SQL and local Win32 streaming access'
+when 3 then 'is enabled for Transact-SQL and both local and remote Win32 streaming access'
+end [FileStream_OS],
+(select value_in_use from sys.configurations where configuration_id = 1580) [FileStream_Instance_value],
+case (select value_in_use from sys.configurations where configuration_id = 1580)
+when 0 then 'is disabled'
+when 1 then 'is enabled for Transact-SQL access'
+when 2 then 'is enabled for Transact-SQL and local Win32 streaming access'
+end [FileStream_Instance],
+case when 
+(select value_in_use from sys.configurations where configuration_id = 1580) != @FilestreamConfiguredLevel
+then 'exec sp_configure ''filestream access level'', '+CAST(configuration_id as varchar(10))+'
+go
+reconfigure with override
+go'
+else 'Already configured'
+end configuration_script
+from (
+values 
+(0, 'Disable'),
+(1, 'Transact-SQL access'),
+(2, 'Full access - Transact-SQL and local Win32 streaming access')) 
+as filestream_config (configuration_id, [description])
+where configuration_id = @FilestreamConfiguredLevel
