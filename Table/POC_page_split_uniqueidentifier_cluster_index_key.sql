5deae7fef9259b7b6dc04fbc8ffe4b16edbc285b
Mohamed <46447150+MohamedAbdelhalem@users.noreply.github.com>
2024-06-09 11:19:44 +0300

    Update and rename POC_page_split_uniqueidentifier_cluster_index_key.sql to POC_page_split_uniqueidentifier_using_NEWID.sql

--- Table/POC_page_split_uniqueidentifier_cluster_index_key.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE seq_test (id INT IDENTITY(1,1), rid UNIQUEIDENTIFIER DEFAULT NEWID(), name VARCHAR(100))
-GO
-INSERT INTO seq_test (name) VALUES ('mohamed')
-GO 1000
-
-SELECT * 
-FROM seq_test 
-ORDER BY rid
---
+++ Table/POC_page_split_uniqueidentifier_using_NEWID.sql
@@ -0,0 +1,23 @@
+--If you want to have a uniqueidentifier as a cluster index key for a table you usually use NEWID() function to generate a new id as a default value
+--However, using this method will lead to frequent page splits, ultimately causing significant overhead. 
+
+CREATE TABLE seq_test_newid (id INT IDENTITY(1,1), rid UNIQUEIDENTIFIER DEFAULT NEWID(), name VARCHAR(100))
+GO
+INSERT INTO seq_test_newid (name) VALUES ('Mohamed')
+GO 1000
+
+SELECT * 
+FROM seq_test_newid 
+ORDER BY rid
+
+--meanwhile, when you use NEWSEQUENTIALID() instead of NEWID() function the behavior will change on the other hand.
+--let's see the changes here.
+
+CREATE TABLE seq_test_newsequentialid` (id INT IDENTITY(1,1), rid UNIQUEIDENTIFIER DEFAULT NEWSEQUENTIALID(), name VARCHAR(100))
+GO
+INSERT INTO seq_test_newsequentialid (name) VALUES ('Mohamed')
+GO 1000
+
+SELECT * 
+FROM seq_test_newsequentialid 
+ORDER BY rid
