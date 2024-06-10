e6778cf2061fc59cf030f394e2105e754e72e432
Mohamed <46447150+MohamedAbdelhalem@users.noreply.github.com>
2024-06-10 14:21:48 +0300

    Update FileTable.sql

--- FileStream/FileTable.sql
+++ FileStream/FileTable.sql
@@ -6,6 +6,9 @@ ALTER DATABASE [AdventureWorks2019] ADD FILE
 (NAME = N'Filestream_F01', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\FFS01' ) 
 TO FILEGROUP [fg_filestream01];
 
+
+ALTER DATABASE AdventureWorks2019 ADD FILEGROUP [fg_filestream02] CONTAINS FILESTREAM;
+GO
 --in multi-folders
 exec xp_cmdshell 'Mkdir "C:\MSSQL\DATA\FFS01"'
 exec xp_cmdshell 'Mkdir "C:\MSSQL\DATA\FFS02"'
@@ -17,24 +20,27 @@ ALTER DATABASE [AdventureWorks2019] ADD FILE
 (NAME = N'Filestream_F02', FILENAME = N'C:\MSSQL\DATA\FFS02\FSTable'), 
 (NAME = N'Filestream_F03', FILENAME = N'C:\MSSQL\DATA\FFS02\FSTable'), 
 (NAME = N'Filestream_F04', FILENAME = N'C:\MSSQL\DATA\FFS02\FSTable') 
-TO FILEGROUP [fg_filestream01];
+TO FILEGROUP [fg_filestream02];
 
---if NON_TRANSACTED_ACCESS = FULL then disable allow the snapshot_isolation_state and is_read_committed_snapshot_on to prevent blocking on filetable
---or use table hint WITH (READCOMMITTEDLOCK)
-select snapshot_isolation_state_desc, is_read_committed_snapshot_on
-from sys.databases where database_id  = DB_ID();
-GO
+
+
 
 ALTER DATABASE AdventureWorks2019  SET FILESTREAM (NON_TRANSACTED_ACCESS = FULL ,DIRECTORY_NAME = 'Deafult_FILESTREAM_directory');
 GO
 
+
+
+--Create FileTable
 CREATE Table Documents AS FILETABLE WITH (
 FileTable_Directory = 'documents', FileTable_Collate_Filename = database_default);
 GO
 
 --Powershell.exe ls "\\127.0.0.1\mssqlserver\Deafult_FILESTREAM_directory\documents"
 --Copy files to this Directory and then select it to see them in database by run the below query
-  
+
+--if NON_TRANSACTED_ACCESS = FULL then disable allow the snapshot_isolation_state and is_read_committed_snapshot_on to prevent blocking on filetable
+--or use table hint WITH (READCOMMITTEDLOCK)
+
 SELECT  
 [stream_id],
 [file_stream],
