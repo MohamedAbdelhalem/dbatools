e731638fa5f89510c60006b3644ffe3131f45b3c
Mohamed <46447150+MohamedAbdelhalem@users.noreply.github.com>
2024-06-12 16:41:23 +0300

    Update generate_database_audit.sql

--- Security/generate_database_audit.sql
+++ Security/generate_database_audit.sql
@@ -1,27 +1,26 @@
 declare 
 @server_audit_loc	varchar(500) = 'C:\backups',
-@drop				varchar(255),
-@p1					varchar(255),
-@p2					varchar(max),
+@drop			varchar(255),
+@p1			varchar(255),
+@p2			varchar(max),
 @audit_script		varchar(max),
-@objects			varchar(max) = '*',
+@objects		varchar(max) = '*',
 @audit_types		varchar(255) = 'SELECT, Insert, Update, Delete',
-@users				varchar(max) = '*',
-@action				int = 1
+@users			varchar(max) = '*',
+@action			int = 1
 --1 = configuration print
 --2 = confgiuration execute
 --3 = select from audit log
 declare
-@SAuditName			varchar(255) = replace(replace(replace(convert(varchar(30),getdate(),120),'-',''),':',''),' ','-'), 
-@audit_log			varchar(500) = @server_audit_loc + case when ltrim(rtrim(right(@server_audit_loc,1))) != '\' then '\' else '' end + '*'
+@SAuditName		varchar(255) = replace(replace(replace(convert(varchar(30),getdate(),120),'-',''),':',''),' ','-'), 
+@audit_log		varchar(500) = @server_audit_loc + case when ltrim(rtrim(right(@server_audit_loc,1))) != '\' then '\' else '' end + '*'
 
 set @SAuditName = 'Audit-'+@SAuditName
 
 if @action in (1,2)
 begin
 
-if exists (select *
-from sys.server_audits)
+if exists (select * from sys.server_audits)
 begin
 
 select top 1
@@ -81,10 +80,6 @@ WITH (STATE = ON)'
 
 if @action = 1
 begin
---print(@drop)
---print(@p1)
---print(@p2)
-
 print(@audit_script)
 end
 else
@@ -99,10 +94,10 @@ set @audit_script = 'USE master
 GO
 CREATE SERVER AUDIT [Audit-20240612-153449]
 TO FILE 
-(	FILEPATH = N'+''''+@server_audit_loc+''''+'
-	,MAXSIZE = 0 MB
-	,MAX_ROLLOVER_FILES = 2147483647
-	,RESERVE_DISK_SPACE = OFF
+(FILEPATH = N'+''''+@server_audit_loc+''''+'
+,MAXSIZE = 0 MB
+,MAX_ROLLOVER_FILES = 2147483647
+,RESERVE_DISK_SPACE = OFF
 ) WITH (QUEUE_DELAY = 1000, ON_FAILURE = CONTINUE)
 GO
 ALTER SERVER AUDIT ['+@SAuditName+'] WITH (STATE = ON)
@@ -124,7 +119,9 @@ else if @action = 3
 begin
 
 --to fetch from the audit file(s)
-select event_time, database_name, session_server_principal_name, succeeded, action_id, is_column_permission,schema_name, object_name, statement, application_name, client_ip  
+select 
+	event_time, database_name, session_server_principal_name, succeeded, action_id, 
+	is_column_permission,schema_name, object_name, statement, application_name, client_ip  
 from sys.fn_get_audit_file(
 @audit_log,
 DEFAULT,
