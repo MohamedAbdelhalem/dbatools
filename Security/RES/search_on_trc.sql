0bba071cc9b197f0765c701a7b8f21401a266be1
Mohamed Abdelhalem <46447150+MohamedAbdelhalem@users.noreply.github.com>
2024-07-07 16:40:54 +0300

    Create search_on_trc.sql

---
+++ Security/RES/search_on_trc.sql
@@ -0,0 +1,37 @@
+declare @log_path varchar(1000), @xp_cmdshell varchar(max), @log varchar(1000)
+declare @log_table table (output_text varchar(max))
+select @log_path = 
+substring(reverse(substring(reverse(value_data), charindex('\',reverse(value_data)), len(value_data))),3,len(value_data))
+from (
+select cast(value_data as varchar(max)) value_data
+from sys.dm_server_registry
+where registry_key = 'HKLM\Software\Microsoft\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQLServer\Parameters'
+and cast(value_data as varchar(max)) like '-e%'
+)a
+set @xp_cmdshell = 'xp_cmdshell '+''''+'dir cd "'+@log_path+'"'''
+insert into @log_table 
+exec(@xp_cmdshell)
+
+declare trc_cursor cursor fast_forward
+for
+select reverse(substring(reverse(ltrim(rtrim(output_text))),1, charindex(' ',reverse(ltrim(rtrim(output_text))))-1)) trc
+from @log_table
+where output_text like '%.trc%'
+order by trc
+
+open trc_cursor
+fetch next from trc_cursor into @log
+while @@FETCH_STATUS = 0
+begin
+
+set @log = @log_path+@log
+
+SELECT StartTime, HostName, ApplicationName, LoginName, TextDAta
+FROM sys.fn_trace_gettable(@log,DEFAULT)   
+WHERE EventClass = 116 
+AND TextData LIKE '%DETACH%'
+
+fetch next from trc_cursor into @log
+end
+close trc_cursor
+deallocate trc_cursor
