a0d6881d98f894cbf316a83cbab615bc6aeea698
Mohamed Abdelhalem <46447150+MohamedAbdelhalem@users.noreply.github.com>
2024-07-10 03:08:55 +0300

    Update search_on_trc.sql

--- Security/RES/search_on_trc.sql
+++ Security/RES/search_on_trc.sql
@@ -4,8 +4,8 @@ select @log_path =
 substring(reverse(substring(reverse(value_data), charindex('\',reverse(value_data)), len(value_data))),3,len(value_data))
 from (
 select cast(value_data as varchar(max)) value_data
-from sys.dm_server_registry
-where registry_key = 'HKLM\Software\Microsoft\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQLServer\Parameters'
+from sys.dm_server_registry r cross apply (select left(cast(value_data as varchar(20)),2) [ver] from sys.dm_server_registry where cast(value_name as varchar(max)) = 'CurrentVersion') v
+where registry_key = 'HKLM\Software\Microsoft\Microsoft SQL Server\MSSQL'+v.ver+'.MSSQLSERVER\MSSQLServer\Parameters'
 and cast(value_data as varchar(max)) like '-e%'
 )a
 set @xp_cmdshell = 'xp_cmdshell '+''''+'dir cd "'+@log_path+'"'''
@@ -14,7 +14,7 @@ exec(@xp_cmdshell)
 
 declare trc_cursor cursor fast_forward
 for
-select reverse(substring(reverse(ltrim(rtrim(output_text))),1, charindex(' ',reverse(ltrim(rtrim(output_text))))-1)) trc
+select max(reverse(substring(reverse(ltrim(rtrim(output_text))),1, charindex(' ',reverse(ltrim(rtrim(output_text))))-1))) trc
 from @log_table
 where output_text like '%.trc%'
 order by trc
