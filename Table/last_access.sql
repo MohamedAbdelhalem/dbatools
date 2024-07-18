ed854f02807b5dc5c7f073971a7a86033b7a8371
Mohamed Abdelhalem <46447150+MohamedAbdelhalem@users.noreply.github.com>
2024-07-18 17:18:33 +0300

    Update last_access.sql

--- Table/last_access.sql
+++ Table/last_access.sql
@@ -16,7 +16,7 @@ select u.database_id, u.object_id,
 convert(varchar(50),last_user_seek,120)  +'_last_user_seek'   last_user_seek,
 convert(varchar(50),last_user_scan,120)  +'_last_user_scan'   last_user_scan,
 convert(varchar(50),last_user_lookup,120)+'_last_user_lookup' last_user_lookup,
-convert(varchar(50),last_user_update,120)+'_last_user_update' last_user_update 
+convert(varchar(50),last_user_update,120)+'_last_user_insert_update_delete' last_user_update 
 from sys.dm_db_index_usage_stats u 
 where u.database_id = db_id()) u
 inner join sys.indexes i
@@ -25,3 +25,11 @@ inner join sys.tables t
 on u.object_id = t.object_id)a
 group by database_name, table_name,
 substring(last_access, charindex('_', last_access)+1, len(last_access)) 
+
+--If the description, last_user_insert_update_delete, has the same last access as the last_user_seek:
+----Afterward, it was noted that users were deleting or updating records using predicates (WHERE condition).
+
+--If description last_user_insert_update_delete alone 
+--Then:
+----1. 99% of it consisted of insert statements.
+----2. 1% of it was updated or deleted without any predicates (WHERE condition), which is a rare case.
