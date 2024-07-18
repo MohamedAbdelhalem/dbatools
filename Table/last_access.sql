c413f99206a4ed2a9f8f71bdbc9bf92634bfe6c3
Mohamed Abdelhalem <46447150+MohamedAbdelhalem@users.noreply.github.com>
2024-07-18 16:48:41 +0300

    Create last_access.sql

---
+++ Table/last_access.sql
@@ -0,0 +1,27 @@
+select database_name, table_name, 
+max(convert(datetime,substring(last_access, 1, charindex('_', last_access)-1),120)) last_access,
+substring(last_access, charindex('_', last_access)+1, len(last_access)) description
+from (
+select db_name(database_id) database_name, '['+schema_name(t.schema_id)+'].['+t.name+']' table_name, (
+	select max(last_access) 
+	from (
+		values 
+			(last_user_seek),
+			(last_user_scan),
+			(last_user_lookup),
+			(last_user_update)
+		 ) as g(last_access)) last_access
+from (
+select u.database_id, u.object_id, 
+convert(varchar(50),last_user_seek,120)  +'_last_user_seek'   last_user_seek,
+convert(varchar(50),last_user_scan,120)  +'_last_user_scan'   last_user_scan,
+convert(varchar(50),last_user_lookup,120)+'_last_user_lookup' last_user_lookup,
+convert(varchar(50),last_user_update,120)+'_last_user_update' last_user_update 
+from sys.dm_db_index_usage_stats u 
+where u.database_id = db_id()) u
+inner join sys.indexes i
+on u.object_id = i.object_id
+inner join sys.tables t
+on u.object_id = t.object_id)a
+group by database_name, table_name,
+substring(last_access, charindex('_', last_access)+1, len(last_access)) 
