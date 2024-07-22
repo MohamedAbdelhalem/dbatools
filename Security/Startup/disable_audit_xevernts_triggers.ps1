c3510061c2039249590f145688a0517a639b8937
Mohamed Abdelhalem <46447150+MohamedAbdelhalem@users.noreply.github.com>
2024-07-23 00:57:36 +0300

    Update disable_audit_xevernts_triggers.ps1

--- Security/Startup/disable_audit_xevernts_triggers.ps1
+++ Security/Startup/disable_audit_xevernts_triggers.ps1
@@ -6,23 +6,45 @@
 #if the script couldn't able to query out the audit list
 #then "Y" to stop the instance and start it with trace falg 3608
 #######################################################
-                                                                                         
-$service = [System.Collections.ArrayList]@()
+
+if (Test-Path variable:service) {rv service}
+if (Test-Path variable:serviceName) {rv serviceName}
+if (Test-Path variable:confirm) {rv confirm}
+if (Test-Path variable:loop) {rv loop}
+if (Test-Path variable:stop_start) {rv stop_start}
+if (Test-Path variable:instance) {rv instance}
+if (Test-Path variable:chosenServiceName) {rv chosenServiceName}
+if (Test-Path variable:chosenInstanceName) {rv chosenInstanceName}
+if (Test-Path variable:ID) {rv ID}
+if (Test-Path variable:table) {rv table}
+if (Test-Path variable:audit) {rv audit}
+if (Test-Path variable:trigger) {rv trigger}
+if (Test-Path variable:ObjectTable) {rv ObjectTable}
+
+$service = [System.Collections.ArrayList]@() 
 $serviceName = [System.Collections.ArrayList]@()
 $table = New-Object System.Collections.ArrayList;
-$auditTable = New-Object System.Collections.ArrayList;
-$service = @(get-service -name "*sql*" | where {$_.DisplayName -like "SQL Server (*"} | select Name | sort DisplayName).name
+$ObjectTable = New-Object System.Collections.ArrayList;
+$service += (get-service -name "*sql*" | where {$_.DisplayName -like "SQL Server (*"} | where-object {$_.Status -eq "Running"} | select Name| sort DisplayName).name
 $audit = [System.Collections.ArrayList]@()
+$trigger = [System.Collections.ArrayList]@()
 
-for ($s = 0; $s -lt $service.count; $s++)
+if ($service.gettype().name -eq "string")
 {
-    if ($service -like "*$*")
+    $serviceName.add($service) | out-null
+}
+else
+{
+    for ($s = 0; $s -lt $service.count; $s++)
     {
-        $serviceName += $service[$s].tostring().substring($service[$s].tostring().indexof("$")+1,$service[$s].length - $service[$s].tostring().indexof("$")-1)
-    }
-    else
-    {
-        $serviceName += $service[$s].tostring()
+        if ($service -like "*$*")
+        {
+            $serviceName += $service[$s].tostring().substring($service[$s].tostring().indexof("$")+1,$service[$s].length - $service[$s].tostring().indexof("$")-1)
+        }
+        else
+        {
+            $serviceName += $service[$s].tostring()
+        }
     }
 }
 
@@ -58,7 +80,7 @@ for ($in = 0; $in -lt $serviceName.count; $in++)
 }
 $table | select  * | Out-Host
 
-$ID = Read-Host -Prompt "Please select an instance ID from the table above to continue."
+$ID = Read-Host -Prompt "Please select an instance ID from the table above to continue"
 $chosenInstanceName = @($table | where {$_.id -eq $ID} | select instancename).instancename 
 $chosenServiceName = @($table | where {$_.id -eq $ID} | select ServiceName).ServiceName
 
@@ -78,66 +100,79 @@ elseif ($exist -eq 1)
     {
         $instance = @(hostname)+"\"+$chosenInstanceName
     }
-
     $audit = @(sqlcmd -S $instance -E -Q "select name from sys.server_audits where is_state_enabled = 1")
-
+    $trigger = @(sqlcmd -S $instance -E -Q "select name from sys.server_triggers where is_disabled = 0")
     if ($stop_start -eq "Y")
     {
         Stop-Service -Name $chosenServiceName
-        Write-Host "Instance ""$chosenServiceName"" has been stopped." -ForegroundColor white -BackgroundColor green
+        Write-Host "Instance ""$chosenServiceName"" has been stopped." -ForegroundColor Black -BackgroundColor green
         net Start $chosenServiceName /T3608
-        Write-Host "The instance ""$chosenServiceName"", has been initiated with the trace flag 3608." -ForegroundColor white -BackgroundColor green
+        Write-Host "The instance ""$chosenServiceName"", has been initiated with the trace flag 3608." -ForegroundColor Black -BackgroundColor green
     }
 
     for ($loop = 0; $loop -lt $audit.count - 4; $loop++)
     {
-        $auditTable += [pscustomobject]@{ID = $loop+1;AuditName = $audit[$loop + 2].Trim(); Status = "Enabled"}
+        $ObjectTable += [pscustomobject]@{ID = $loop + 1;Name = $audit[$loop + 2].Trim(); Type = "Audit"; Status = "Enabled"}
     } 
-    $auditTable | select * | Out-Host
-    $existAduit = @($auditTable).Count
-    if ($existAduit -eq 0)
+    $id = $ObjectTable.count
+    for ($loop = 0; $loop -lt $trigger.count - 4; $loop++)
     {
-        Write-Host "In instance ""$chosenInstanceName"", there are no enabled audits to be disabled." -ForegroundColor Green
+        $ObjectTable += [pscustomobject]@{ID = $id + $loop + 1;Name = $trigger[$loop + 2].Trim(); Type = "Trigger"; Status = "Enabled"}
+    } 
+    $ObjectTable | select * | Out-Host
+    $existObject = @($auditTable).Count
+    if ($existObject -eq 0)
+    {
+        Write-Host "In instance ""$chosenInstanceName"", there are no enabled audits or triggers to be disabled." -ForegroundColor Green
     }
     elseif ($existAduit -gt 0)
     {
-        $confirm = Read-Host -Prompt "Would you like to disable the audit list above? Please type ""Confirm"" to proceed or ""Press any key"" to skip and ignore."
-        if ($confirm -eq "Confirm")
+        $confirm = Read-Host -Prompt "what do you want to disable from the above list? Please type ""Trigger"", ""Audit"", or ""ALL"" or ""Press any key"" to skip and ignore."
+        if ($confirm -eq "trigger" -or $confirm -eq "all")
+        {
+            try 
+            {
+                for ($loop = 0; $loop -lt $trigger.count - 4; $loop++)
+                {
+                    $command = "DISABLE TRIGGER ["+$trigger[$loop + 2].Trim()+"] ON ALL SERVER;"
+                    $command 
+                    sqlcmd -S $instance -E -Q $command
+                }
+                Write-Host "The trigger list has been successfully disabled." -ForegroundColor Green
+             }
+             catch 
+             {
+                Write-Host "There was an issue while trying to disable the triggers. Please troubleshoot and try again." -ForegroundColor Red
+             }
+        }
+        if ($confirm -eq "audit" -or $confirm -eq "all")
         {
             try 
             {
                 for ($loop = 0; $loop -lt $audit.count - 4; $loop++)
                 {
                     $command = "ALTER SERVER AUDIT ["+$audit[$loop + 2].Trim()+"] WITH (STATE=OFF);"
+                    $command 
                     sqlcmd -S $instance -E -Q $command
                 }
                 Write-Host "The audit list has been successfully disabled." -ForegroundColor Green
              }
              catch 
              {
-                Write-Host "There was an issue while trying to disable the audit. Please troubleshoot and try again." -ForegroundColor Red
+                Write-Host "There was an issue while trying to disable the audits. Please troubleshoot and try again." -ForegroundColor Red
              }
         }
     }
     if ($stop_start -eq "Y")
     {
         Stop-Service -Name $chosenServiceName
-        Write-Host "Instance ""$chosenServiceName"" has been stopped." -ForegroundColor white -BackgroundColor green
+        Write-Host "Instance ""$chosenServiceName"" has been stopped." -ForegroundColor Black -BackgroundColor green
         Start-Service -Name $chosenServiceName
-        Write-Host "Instance ""$chosenServiceName"" has been started." -ForegroundColor white -BackgroundColor green
+        Write-Host "Instance ""$chosenServiceName"" has been started normally." -ForegroundColor Black -BackgroundColor green
     }
 }
 else
 {
     Write-Host "Something went wrong." -ForegroundColor Red
+
 }
-
-#rv confirm
-#rv loop
-#rv stop_start
-#rv instance
-#rv chosenServiceName
-#rv chosenInstanceName
-#rv ID
-#rv table
-
