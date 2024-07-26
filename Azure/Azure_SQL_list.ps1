b512fe96a4a93e96d44b1f600fbd0dd7c4920523
Mohamed Abdelhalem <46447150+MohamedAbdelhalem@users.noreply.github.com>
2024-07-26 16:48:47 +0300

    Create Azure_SQL_list.ps1

---
+++ Azure/Azure_SQL_list.ps1
@@ -0,0 +1,18 @@
+name 
+status
+creationDate                      : 4/27/2024 10:38:24 AM
+minCapacity                       : 0.5
+maxSizeBytes                      : 2147483648
+maxLogSizeBytes                   : 193273528320
+autoPauseDelay 
+collation                         : SQL_Latin1_General_CP1_CI_AS
+createMode                        : 
+currentBackupStorageRedundancy    : Geo
+currentServiceObjectiveName
+zoneRedundant 
+type
+manualCutover                     : 
+requestedBackupStorageRedundancy  : Local
+requestedServiceObjectiveName
+readScale
+managedBy                         : 
