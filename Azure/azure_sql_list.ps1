0b58631fbdae78ffb1bb8b5e419b653646d2d83f
Mohamed Abdelhalem <46447150+MohamedAbdelhalem@users.noreply.github.com>
2024-07-26 17:11:11 +0300

    Update azure_sql_list.ps1

--- Azure/azure_sql_list.ps1
+++ Azure/azure_sql_list.ps1
@@ -4,25 +4,26 @@ $server = $azserver | ConvertFrom-Json
 $sqlList = az sql db list --resource-group $server.resourceGroup --server $server.name
 $azuresql = $sqlList | ConvertFrom-Json
 
+for($i = 0; $i -lt $azuresql.name.count + 1; $i++)
+{
 $azsql += [pscustomobject]@{
-database_name = $azuresql.name;
-status = $azuresql.status;
-creationDate = $azuresql.creationDate;
-minCapacity = $azuresql.minCapacity;
-maxSizeBytes = $azuresql.maxSizeBytes;
-maxLogSizeBytes = $azuresql.maxLogSizeBytes;
-autoPauseDelay = $azuresql.autoPauseDelay;
-collation = $azuresql.collation;
-createMode = $azuresql.createMode;
-currentBackupStorageRedundancy = $azuresql.currentBackupStorageRedundancy;
-currentServiceObjectiveName = $azuresql.currentServiceObjectiveName;
-zoneRedundant = $azuresql.zoneRedundant;
-type = $azuresql.type;
-manualCutover = $azuresql.manualCutover;
-requestedBackupStorageRedundancy = $azuresql.requestedBackupStorageRedundancy;
-requestedServiceObjectiveName = $azuresql.requestedServiceObjectiveName;
-readScale = $azuresql.readScale;
-managedBy = $azuresql.managedBy;
+database_name = $azuresql.name[$i];
+status = $azuresql.status[$i];
+creationDate = $azuresql.creationDate[$i];
+minCapacity = $azuresql.minCapacity[$i];
+maxSizeBytes = $azuresql.maxSizeBytes[$i];
+maxLogSizeBytes = $azuresql.maxLogSizeBytes[$i];
+autoPauseDelay = $azuresql.autoPauseDelay[$i];
+collation = $azuresql.collation[$i];
+currentBackupStorageRedundancy = $azuresql.currentBackupStorageRedundancy[$i];
+currentServiceObjectiveName = $azuresql.currentServiceObjectiveName[$i];
+zoneRedundant = $azuresql.zoneRedundant[$i];
+type = $azuresql.type[$i];
+manualCutover = $azuresql.manualCutover[$i];
+requestedBackupStorageRedundancy = $azuresql.requestedBackupStorageRedundancy[$i];
+requestedServiceObjectiveName = $azuresql.requestedServiceObjectiveName[$i];
+readScale = $azuresql.readScale[$i];
+managedBy = $azuresql.managedBy[$i]}
 }
 
 $azsql | format-table 
