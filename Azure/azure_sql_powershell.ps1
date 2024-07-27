a82cfeb7c76ca46b564d709bcac5eef116f57539
Mohamed Abdelhalem <46447150+MohamedAbdelhalem@users.noreply.github.com>
2024-07-27 14:50:05 +0300

    Update azure_sql_powershell.ps1

--- Azure/azure_sql_powershell.ps1
+++ Azure/azure_sql_powershell.ps1
@@ -5,5 +5,5 @@
 $resourceGroupList = Get-AzureRmResourceGroup
 $server = Get-AzureRmSqlServer
 $azureSql = Get-AzureRmSqlDatabase -ResourceGroupName $resourceGroupList.ResourceGroupName -ServerName $server.ServerName
-$azureSql | select DatabaseName, Location, Edition,@{N='MaxSizeGB';E={[string]($_.MaxSizeBytes/1GB) + " GB"}}, Status,
+$azureSql | select DatabaseName, Location, Edition,@{N='MaxSize';E={[string]($_.MaxSizeBytes/1GB) + " GB"}}, Status,
 currentServiceObjectiveName,zoneRedundant,readScale,managedBy  | format-table
