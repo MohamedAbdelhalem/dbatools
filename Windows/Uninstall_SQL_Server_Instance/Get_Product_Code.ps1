3b378027b4cf997973c1d3f93ce0e42032d37e1a
Mohamed Abdelhalem <46447150+MohamedAbdelhalem@users.noreply.github.com>
2024-08-03 13:45:53 +0300

    Update Get_Product_Code.ps1

--- Windows/Uninstall_SQL_Server_Instance/Get_Product_Code.ps1
+++ Windows/Uninstall_SQL_Server_Instance/Get_Product_Code.ps1
@@ -6,12 +6,24 @@ if (Test-Path variable:table) {rv table}
 $path = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Microsoft SQL Server\"
 $productPath = @(Get-ChildItem -Path "Registry::$path" -recurse  | where {$_.property -like "ProductCode"} | select name)
 $table = New-Object System.Collections.ArrayList;
+$sqlserivces = get-service -name "*sql*" | where {$_.DisplayName -like "*SQL Server (*"} | select name
+$sqlserivces | foreach-object {$_.name}
 
 for ($pc = 0; $pc -lt $productPath.Count; $pc++)
 {
+    
     $name= ($productPath | select name | select-object -index $pc).Name
+    $sqlserivces | foreach-object {$name2 = $name | select-string $_.name; $name3 = $_.name}
     $productcode = (Get-ItemProperty -Path "Registry::$name" | select productcode).productcode
-    $table += [pscustomobject]@{ID = $pc;path = $name; productcode = $productcode}
+    if ($name -eq $name2)
+    {
+        $table += [pscustomobject]@{ID = $pc;path = $name; productcode = $productcode; Instance = $name3}
+    }
+    else
+    {
+        $table += [pscustomobject]@{ID = $pc;path = $name; productcode = $productcode; Instance = "Shared"}
+    }
 }
 
-$table
+$table | Format-Table
+
