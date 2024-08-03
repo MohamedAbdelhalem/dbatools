c319d165cad3b64865724adea4a0f293b82d009a
Mohamed Abdelhalem <46447150+MohamedAbdelhalem@users.noreply.github.com>
2024-08-03 15:27:04 +0300

    Update Get_Product_Code.ps1

--- Windows/Uninstall_SQL_Server_Instance/Get_Product_Code.ps1
+++ Windows/Uninstall_SQL_Server_Instance/Get_Product_Code.ps1
@@ -1,29 +1,25 @@
+$FormatEnumerationLimit = -1
+
 if (Test-Path variable:name) {rv name}
 if (Test-Path variable:productPath) {rv productPath}
 if (Test-Path variable:productcode) {rv productcode}
 if (Test-Path variable:table) {rv table}
+if (Test-Path variable:sqlserivces) {rv sqlserivces}
 
 $path = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Microsoft SQL Server\"
 $productPath = @(Get-ChildItem -Path "Registry::$path" -recurse  | where {$_.property -like "ProductCode"} | select name)
 $table = New-Object System.Collections.ArrayList;
+$instances = New-Object System.Collections.ArrayList;
 $sqlserivces = get-service -name "*sql*" | where {$_.DisplayName -like "*SQL Server (*"} | select name
-$sqlserivces | foreach-object {$_.name}
+$ins = $sqlserivces | foreach-object {if($_.name -like  "*$*"){$_.name.Substring(6,$_.name.Length-6)}else{$_.name}}
+$ins  | foreach-object {$instances += [pscustomobject]@{Name = $_}}
 
 for ($pc = 0; $pc -lt $productPath.Count; $pc++)
 {
-    
     $name= ($productPath | select name | select-object -index $pc).Name
-    $sqlserivces | foreach-object {$name2 = $name | select-string $_.name; $name3 = $_.name}
+    $serviceName = $instances | foreach-object {if ($name.Contains("."+$_.name+"\")){$_.name}}
     $productcode = (Get-ItemProperty -Path "Registry::$name" | select productcode).productcode
-    if ($name -eq $name2)
-    {
-        $table += [pscustomobject]@{ID = $pc;path = $name; productcode = $productcode; Instance = $name3}
-    }
-    else
-    {
-        $table += [pscustomobject]@{ID = $pc;path = $name; productcode = $productcode; Instance = "Shared"}
-    }
+    $table += [pscustomobject]@{ID = $pc;path = $name; productcode = $productcode; Instance = $serviceName}
 }
 
-$table | Format-Table
-
+$table | Format-Table -AutoSize
