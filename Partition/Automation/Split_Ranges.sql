a8cc84d38b235a76446b0c2b24547606bcab554e
Mohamed Abdelhalem <46447150+MohamedAbdelhalem@users.noreply.github.com>
2024-08-17 02:03:00 +0300

    Update Split_Ranges.sql

--- Partition/Automation/Split_Ranges.sql
+++ Partition/Automation/Split_Ranges.sql
@@ -1,10 +1,11 @@
 --parameters
 declare 
-@table_name			varchar(255) = '[Sales].[SalesOrderHeader]',
-@bulk				decimal(12,0) = 1000,
-@partition_column	varchar(255) = 'SalesOrderID',
-@useLastFileGroup	int = 1,
-@manualFileGroup	varchar(500) = 'default'
+@table_name		varchar(255)  = '[Sales].[SalesOrderHeader]',
+@add_ranges_on		char(1)	      = 'L', --accepted values "R" for Right or "L" for Left
+@bulk			decimal(12,0) = 1000,
+@partition_column	varchar(255)  = 'SalesOrderID',
+@useLastFileGroup	int 	      = 1,
+@manualFileGroup	varchar(500)  = 'DL006'
 
 --variables
 declare
@@ -12,18 +13,21 @@ declare
 @partition_scheme	varchar(255),
 @partition_rows		varchar(255),
 @partition_from		varchar(255),
-@sql				nvarchar(max),
+@Partition_Key		varchar(255),
+@sql			nvarchar(max),
 @biggest_value		bigint,
-@loop				int = 1,
-@psname				varchar(500),
-@fgname				varchar(500),
-@pscount			int
+@smallest_value		bigint,
+@loop			int = 1,
+@psname			varchar(500),
+@fgname			varchar(500),
+@pscount		int
 
 set nocount on
 select 
-@partition_function = partition_function,
+@partition_function	= partition_function,
 @partition_scheme	= partition_scheme,
-@partition_from		= Partition_Value_From
+@partition_from		= Partition_Value_From,
+@Partition_Key		= Partition_Key_Value
 from (
 select 
 '['+cast(pf.name as varchar(500))+']' Partition_Function, 
@@ -32,7 +36,9 @@ master.dbo.numbersize(sum(total_pages) over(partition by al.table_name) *8.0,'k'
 isnull((prv.boundary_id + boundary_value_on_right),al.partition_number) partition_number, 
 cast(prv.value as varchar(255)) Partition_Key_Value, 
 cast(LAG(prv.value,1,1) OVER(ORDER BY table_name, partition_number) as varchar(255)) Partition_Value_From,
-case when prv.value is null then '>' else prv.value end Partition_Value_To
+case when prv.value is null then '>' else prv.value end Partition_Value_To,
+min(partition_number) over() first_partition,
+max(partition_number) over() last_partition
 from (
 select i.data_space_id, p.object_id, p.index_id,
 '['+schema_name(schema_id)+'].['+t.name+']' table_name,partition_number,
@@ -57,20 +63,23 @@ on prv.function_id = pf.function_id
 and (prv.boundary_id + boundary_value_on_right) = al.partition_number
 where table_name = @table_name
 )x
-where Partition_Key_Value is null
+where partition_number = case @add_ranges_on when 'R' then last_partition when 'L' then first_partition end
 order by table_name, partition_number
 
-
+if @add_ranges_on = 'R'
+begin
 set @sql = N'Select Top 1 @output = max('+@partition_column+') FROM '+@table_name+' Where '+@partition_column+' > '+cast(@partition_from as varchar(50))
 exec sp_executesql @sql, N'@output decimal(12,0) output', @output = @biggest_value output
+end
 
 set @loop = 0
-
 select @pscount = count(*)
 from sys.partition_schemes ps inner join sys.partition_functions pf
 on ps.function_id = pf.function_id
 where pf.name = replace(replace(@partition_function,']',''),'[','')
 
+if @add_ranges_on = 'R'
+begin
 while @partition_from < @biggest_value
 begin
 	set @loop = 1
@@ -96,15 +105,54 @@ begin
 		where pfname = replace(replace(@partition_function,']',''),'[','')
 		and id = @loop
 
-		print('ALTER PARTITION SCHEME '+@psname+' NEXT USED ['+case when @useLastFileGroup = 1 then @fgname end +'];')  
+		print('ALTER PARTITION SCHEME '+@psname+' NEXT USED ['+case when @useLastFileGroup = 1 then @fgname else @manualFileGroup end +'];')  
 		print('GO')
 
 		set @loop += 1
 	end
 
-	select @partition_from = @partition_from + @bulk
+	select @partition_from = @partition_from + @bulk 
 	print('ALTER PARTITION FUNCTION '+@partition_function+'() SPLIT RANGE ('+cast(@partition_from as varchar(200))+');')  
 	print('GO')
 end
-set nocount off
+end
+else
+if @add_ranges_on = 'L'
+begin
+	while (cast(@Partition_Key as bigint) - @bulk) > 0
+	begin
+		set @loop = 1
+		while @loop < @pscount + 1
+		begin
+			select 
+			@psname = psname, 
+			@fgname = fgname
+			from (
+			select 
+			row_number() over(order by ps.name) id,
+			maxfg  = max(d.destination_id), 
+			psname = ps.name, 
+			fgname = fg.name,
+			pfname = pf.name
+			from sys.filegroups fg inner join sys.destination_data_spaces d
+			on fg.data_space_id = d.data_space_id
+			inner join sys.partition_schemes ps
+			on ps.data_space_id = d.partition_scheme_id
+			inner join sys.partition_functions pf
+			on ps.function_id = pf.function_id
+			group by ps.name, fg.name, pf.name)a
+			where pfname = replace(replace(@partition_function,']',''),'[','')
+			and id = @loop
 
+			print('ALTER PARTITION SCHEME '+@psname+' NEXT USED ['+case when @useLastFileGroup = 1 then @fgname else @manualFileGroup end +'];')  
+			print('GO')
+			set @loop += 1
+		end
+
+		set @Partition_Key -= @bulk
+		print('ALTER PARTITION FUNCTION '+@partition_function+'() SPLIT RANGE ('+cast(@Partition_Key as varchar(200))+');')  
+		print('GO')
+	end
+end
+
+set nocount off
