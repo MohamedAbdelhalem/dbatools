6ba8d0688956ef6fcaff292f566b5fa1a438cbf6
Mohamed Abdelhalem <46447150+MohamedAbdelhalem@users.noreply.github.com>
2024-08-20 13:19:27 +0300

    Update check.sql

--- AlwaysOn/EndPoint/check.sql
+++ AlwaysOn/EndPoint/check.sql
@@ -18,3 +18,18 @@ ALTER AUTHORIZATION ON availability group::[agname] TO [newowner]
 USE master;
 SELECT SUSER_NAME(principal_id) AS endpoint_owner, name AS endpoint_name
 FROM sys.database_mirroring_endpoints;
+
+
+USE [master]
+GO
+SELECT pm.class, pm.class_desc, pm.major_id, pm.minor_id, 
+   pm.grantee_principal_id, pm.grantor_principal_id, 
+   pm.[type], pm.[permission_name], pm.[state],pm.state_desc, 
+   pr.[name] AS [owner], gr.[name] AS grantee, e.[name] AS endpoint_name
+FROM sys.server_permissions pm 
+   JOIN sys.server_principals pr ON pm.grantor_principal_id = pr.principal_id
+   JOIN sys.server_principals gr ON pm.grantee_principal_id = gr.principal_id
+   JOIN sys.endpoints e ON pm.grantor_principal_id = e.principal_id 
+        AND pm.major_id = e.endpoint_id
+WHERE pm.class = 'endpoint';
+
