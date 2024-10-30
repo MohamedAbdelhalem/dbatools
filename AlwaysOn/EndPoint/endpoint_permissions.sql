c918b047301d8e7e1ca723bc8c721ec8eecfdb78
Mohamed Abdelhalem <46447150+MohamedAbdelhalem@users.noreply.github.com>
2024-10-30 16:17:02 +0300

    Update endpoint_permissions.sql

--- AlwaysOn/EndPoint/endpoint_permissions.sql
+++ AlwaysOn/EndPoint/endpoint_permissions.sql
@@ -1,9 +1,11 @@
 --grant
-select ep.endpoint_id, ep.name, ep.state_desc, sp.class_desc, sp.grantee_principal_id, spr.name, 
+select ep.endpoint_id, ep.name, ep.state_desc, sp.class_desc, sp.grantee_principal_id, spr.name, l.sysadmin, 
 sp.permission_name, sp.state_desc, ep.protocol_desc,
 sp.state_desc+' '+[permission_name]+' ON '+class_desc+'::['+ep.name+'] TO [' collate Latin1_General_CI_AS+spr.name+']' permission_script
 from sys.endpoints ep left outer join sys.server_permissions sp 
 on sp.major_id = ep.endpoint_id
 left outer join sys.server_principals spr
 on sp.grantee_principal_id = spr.principal_id
+left outer join sys.syslogins l
+on spr.name = l.name
 where ep.endpoint_id > 5
