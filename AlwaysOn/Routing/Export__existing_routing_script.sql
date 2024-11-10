8014f2f1bbf59e807b1cdcc8391cdb393a6faf1a
Mohamed Abdelhalem <46447150+MohamedAbdelhalem@users.noreply.github.com>
2024-11-10 20:21:56 +0300

    Create Export__existing_routing_script.sql

---
+++ AlwaysOn/Routing/Export__existing_routing_script.sql
@@ -0,0 +1,32 @@
+select replica_id, replica_server_name, [READ_ONLY_ROUTING_LIST],
+'ALTER AVAILABILITY GROUP [AG] MODIFY REPLICA ON N'+''''+replica_server_name+''''+' WITH (PRIMARY_ROLE (READ_ONLY_ROUTING_LIST='+[READ_ONLY_ROUTING_LIST]+'));'
+from (
+select replica_id, replica_server_name, '(' +
+isnull([1],    '') + isnull(','+[2],'') + isnull(','+[3],'') + isnull(','+[4],'') + 
+isnull(','+[5],'') + isnull(','+[6],'') + isnull(','+[7],'') + isnull(','+[8],'') + isnull(','+[9],'') + 
+isnull(','+[10],'') + isnull(','+[11],'') + isnull(','+[12],'') + isnull(','+[13],'') + ')' [READ_ONLY_ROUTING_LIST]
+from (
+select replica_id, replica_server_name, routing_priority, 
+case when [2] is not null then '(' else '' end + 
+isnull(''''+[1]+'''',    '') + isnull(','+''''+[2]+'''','') + isnull(','+''''+[3]+'''','') + isnull(','+''''+[4]+'''','') + 
+isnull(','+''''+[5]+'''','') + isnull(','+''''+[6]+'''','') + isnull(','+''''+[7]+'''','') + isnull(','+''''+[8]+'''','') + isnull(','+''''+[9]+'''','') + 
+isnull(','+''''+[10]+'''','') + isnull(','+''''+[11]+'''','') + isnull(','+''''+[12],'') + isnull(','+''''+[13]+'''','') + case when [2] is not null then ')' else '' end [READ_ONLY_ROUTING_LIST]
+from (
+select 
+dense_rank()over(order by ar1.replica_server_name) replica_id, 
+ar1.replica_server_name, 
+row_number()over(partition by ar1.replica_server_name,ro.routing_priority order by ar1.replica_server_name, ro.routing_priority) id, 
+ro.routing_priority, ar2.replica_server_name [READ_ONLY_ROUTING_LIST]
+from dbo.availability_read_only_routing_lists_test_balance ro 
+inner join dbo.availability_replicas_test ar1
+on ro.replica_id = ar1.replica_id
+inner join dbo.availability_replicas_test ar2
+on ro.read_only_replica_id = ar2.replica_id
+where ro.flag = 1)a
+pivot(
+max([READ_ONLY_ROUTING_LIST]) for id in ([1],[2],[3],[4],[5],[6],[7],[8],[9],[10],[11],[12],[13]))p1
+)b
+pivot(
+max([READ_ONLY_ROUTING_LIST]) for routing_priority in ([1],[2],[3],[4],[5],[6],[7],[8],[9],[10],[11],[12],[13]))p2
+)c
+order by replica_id
