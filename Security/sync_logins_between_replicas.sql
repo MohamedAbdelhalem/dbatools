f7990fc4b0405ed7b36cf644c5601109ea931f54
Mohamed Abdelhalem <46447150+MohamedAbdelhalem@users.noreply.github.com>
2024-12-08 14:25:51 +0300

    Update sync_logins_between_replicas.sql

--- Security/sync_logins_between_replicas.sql
+++ Security/sync_logins_between_replicas.sql
@@ -195,7 +195,7 @@ cast(replace(convert(varchar(10),dateadd(minute,1,getdate()),108),':','') as big
 @scheduleid = jsch.schedule_id 
 from msdb.dbo.sysjobs j inner join msdb.dbo.sysjobschedules jsch
 on j.job_id = jsch.job_id
-where j.name = 'sync_logins'
+where j.name = 'sync_logins_job'
 
 EXEC msdb.dbo.sp_update_schedule 
 @schedule_id=@scheduleid, 
@@ -208,49 +208,14 @@ END;
 
 GO
 
-USE [msdb]
-GO
-
-/****** Object:  Job [sync_logins]    Script Date: 12/6/2024 11:39:32 PM ******/
-BEGIN TRANSACTION
-DECLARE @ReturnCode INT
-SELECT @ReturnCode = 0
-/****** Object:  JobCategory [[Uncategorized (Local)]]    Script Date: 12/6/2024 11:39:32 PM ******/
-IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'[Uncategorized (Local)]' AND category_class=1)
-BEGIN
-EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'[Uncategorized (Local)]'
-IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-
-END
-
-DECLARE @jobId BINARY(16)
-EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'sync_logins', 
-		@enabled=1, 
-		@notify_level_eventlog=0, 
-		@notify_level_email=0, 
-		@notify_level_netsend=0, 
-		@notify_level_page=0, 
-		@delete_level=0, 
-		@description=N'No description available.', 
-		@category_name=N'[Uncategorized (Local)]', 
-		@owner_login_name=N'sa', @job_id = @jobId OUTPUT
-IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [s1]    Script Date: 12/6/2024 11:39:33 PM ******/
-EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N's1', 
-		@step_id=1, 
-		@cmdexec_success_code=0, 
-		@on_success_action=1, 
-		@on_success_step_id=0, 
-		@on_fail_action=2, 
-		@on_fail_step_id=0, 
-		@retry_attempts=0, 
-		@retry_interval=0, 
-		@os_run_priority=0, @subsystem=N'TSQL', 
-		@command=N'declare @replica_server_name varchar(300)
-IF (select role_desc 
-	from sys.dm_hadr_availability_replica_states
-	where is_local = 1) = ''PRIMARY''
-BEGIN
+CREATE Procedure dbo.apply_sync_logins
+as
+begin
+declare @replica_server_name varchar(300)
+if (select role_desc 
+      from sys.dm_hadr_availability_replica_states
+      where is_local = 1) = 'PRIMARY'
+begin
 	declare replica_cursor cursor fast_forward
 	for
 	select rcs.replica_server_name 
@@ -262,42 +227,28 @@ BEGIN
 	fetch next from replica_cursor into @replica_server_name
 	while @@FETCH_STATUS = 0
 	begin
-	exec [dbo].[sync_logins_between_replicas] @show=''sync'',@replica_name=@replica_server_name
+	exec [dbo].[sync_logins_between_replicas] @show='sync',@replica_name=@replica_server_name
 	fetch next from replica_cursor into @replica_server_name
 	end
 	close replica_cursor
 	deallocate replica_cursor
-END
-', 
-		@database_name=N'master', 
-		@flags=0
-IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
-IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'sch_sync_one_time', 
-		@enabled=0, 
-		@freq_type=1, 
-		@freq_interval=0, 
-		@freq_subday_type=0, 
-		@freq_subday_interval=0, 
-		@freq_relative_interval=0, 
-		@freq_recurrence_factor=0, 
-		@active_start_date=20241207, 
-		@active_end_date=99991231, 
-		@active_start_time=3828, 
-		@active_end_time=235959, 
-		@schedule_uid=N'1d9709c4-26b0-4aea-b999-d59a254eabb3'
-IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N'(local)'
-IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-COMMIT TRANSACTION
-GOTO EndSave
-QuitWithRollback:
-    IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
-EndSave:
+end
+end
 GO
 
-
-
-
+--create a new job on each replica including the primary node for this job name sync_logins_job and if you need to change the name of this job change it on the above trigger too.
+USE [msdb]
+GO
+EXEC msdb.dbo.sp_add_job @job_name=N'sync_logins_job', 
+		@enabled=1, 
+		@notify_level_eventlog=0, 
+		@notify_level_email=0, 
+		@notify_level_netsend=0, 
+		@notify_level_page=0, 
+		@delete_level=0, 
+		@description=N'No description available.', 
+		@category_name=N'[Uncategorized (Local)]', 
+		@owner_login_name=N'sa'
+--then add this procedure master.dbo.apply_sync_logins in the step
+--add scheduler to run one time and disable the scheduler
 
